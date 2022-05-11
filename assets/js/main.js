f15f6ba4816bab6ff0036f9e1d5adcc47af4612a
Riceball <42161586+Anderice@users.noreply.github.com>
2022-05-11 15:45:01 +0800

    gsap added

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const image = gsap.timeline();
+const text = gsap.timeline();
+const list = gsap.timeline();
+const cta = gsap.timeline();
+
+list.from(".gsap-list", {opacity: 0,translateY: 30, duration: .8, stagger: .1, ease: "power4.out"});
+
+image.from(".gsap-image", {opacity: 0,translateY: 100, duration: .8, stagger: .1, ease: "power4.out"});
+
+text.from(".gsap-heading", {opacity: 0,translateY: 100, duration: .8, stagger: .1, ease: "power4.out"});
+
+cta.from(".gsap-cta", {opacity: 0, duration: 1,delay: .4, stagger: .5, ease: "power4.out"});
+
