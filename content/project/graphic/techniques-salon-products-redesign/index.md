6858701bc9476f0952c2306dbb5fc51e5e025602
Riceball <42161586+Anderice@users.noreply.github.com>
2022-05-13 07:37:46 +0800

    post meta

--- content/project/graphic/techniques-salon-products-redesign/index.md
+++ content/project/graphic/techniques-salon-products-redesign/index.md
@@ -1,5 +1,10 @@
 ---
 draft: false
+cover:
+  - https://ucarecdn.com/cfc19b0c-3db8-4f75-99c6-4367b53db06a/01-克比提護髮素與洗髮精.webp
+  - https://ucarecdn.com/553a4695-9bc6-40a8-9ded-bf192669b51d/02-克比提護髮素與洗髮精瓶身主圖動畫.webp
+  - https://ucarecdn.com/b4fb5cf8-e03c-48e6-959b-2e5fca877633/03-克比提護髮素與洗髮精瓶身3種擺放方式動畫.webp
+  - https://ucarecdn.com/c3596918-4fea-467a-bea4-6fe5f7393134/04-克比提護髮素與洗髮精橫幅.jpg
 title: Techniques Salon products redesign
 titletc: 克比提護髮素與洗髮精瓶身設計
 description: Techniques Salon products redesign
@@ -7,6 +12,10 @@ project:
   - 平面
 date: 2022-03-11T21:36:35.630Z
 client: 三櫻國際髮藝
-credit: []
+credit:
+  - title: 平面設計
+    person: 黃宗瑋、王耀輝
+  - title: 文案
+    person: 王耀輝、陳奕杉
 ---
 新
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,36 @@ module.exports = {
         transitionProperty: {
             'background': 'background',
         },
-        typography: {
-            DEFAULT: {
-                css: {
-                    "code::before": {content: ''},
-                    "code::after": {content: ''},
-                }
-            }
-        }
+
+        typography: (theme) => ({
+          DEFAULT: {
+            css: {
+              "code::before": {content: ''},
+              "code::after": {content: ''},
+              h2: {
+                backgroundColor: theme('colors.stone.100'),
+                paddingTop: theme('padding.4'),
+                paddingBottom: theme('padding.4'),
+                paddingLeft: theme('padding.8'),
+                paddingRight: theme('padding.8'),
+                fontWeight: 400,
+                fontSize: theme('fontSize.lg'),
+                position: "sticky",
+                top: "var(--navbarMainHeight)",
+              },
+              p: {
+                paddingLeft: theme('padding.8'),
+                paddingRight: theme('padding.8'),
+                maxWidth: theme('maxWidth.prose')
+              },
+              ul: {
+                paddingLeft: theme('padding.8'),
+                paddingRight: theme('padding.8'),
+                listStylePosition: "inside",
+              }
+            },
+          },
+        }),
     }
 },
   plugins: [
