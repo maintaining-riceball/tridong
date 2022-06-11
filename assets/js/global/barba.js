8ed12d964911113f15d247d7daf397338cb7984b
Riceball <42161586+Anderice@users.noreply.github.com>
2022-06-11 20:37:13 +0800

    barba.js

--- assets/js/global/barba.js
+++ assets/js/global/barba.js
@@ -4,26 +4,21 @@ barba.use(barbaPrefetch);
 
 function leave() {
     var leave = gsap.timeline();
-    console.log('leave')
     leave.to('body', {overflow:"hidden"}, 0)
          .to('.barba-loading', {display: "flex",opacity: 1, duration: 0.75, ease: "power4.out"}, 0)
 }
 
 function enter() {
     const enter = gsap.timeline();
-    console.log('enter')
     enter.to("body", {overflow: "auto"}, 0)
          .to('.barba-loading', {display: "none", opacity: 0, duration: 0.75}, 0)
-
          .from(".gsap-heading", {opacity: 0,translateY: 100, duration: .8, stagger: .1, ease: "power4.out"}, 0)
          .from(".gsap-cta", {opacity: 0, duration: 1,delay: .2, stagger: .5, ease: "power4.out"}, 0)
          .from(".gsap-list", {opacity: 0,translateY: 30, duration: .8, stagger: .1, ease: "power4.out"}, "-1.2")
-
 }
 
 function once() {
     const once = gsap.timeline();
-    console.log('enter')
     once.from(".gsap-heading", {opacity: 0,translateY: 100, duration: .8, stagger: .1, ease: "power4.out"}, 0)
         .from(".gsap-list", {opacity: 0,translateY: 30, duration: .8, stagger: .1, ease: "power4.out"}, 0)
         .from(".gsap-cta", {opacity: 0, duration: 1,delay: .2, stagger: .5, ease: "power4.out"}, 0)
@@ -44,25 +39,27 @@ barba.init({
         namespace: 'Home',
         beforeEnter() {
             matterIndex();
-            console.log('beforeEnter-matterIndex()');
         }
     }],
     transitions: [{
         name: 'default-transition',
         leave(data) {
-            leave();
+            // leave();
+            console.log('Basic Leave');
         },
 
         async enter(data) {
             const done = this.async();
             await delay(1500);
-            enter();
+            // enter();
+            console.log('Basic Enter');
             window.scrollTo(0, 0);
             done();
         },
 
         once(data) {
             once();
+            console.log('Basic Once');
         },
         
         before: ({ current, next, trigger }) => {
@@ -72,8 +69,6 @@ barba.init({
             let menu = document.querySelector('#navlistMain');
 
             // select the menu item depending on the next URL (you can do that in many ways)
-            console.log(menu)
-            console.log(next)
             let nextItem = menu.querySelector(`a[href="${next.url.path}"]`);
     
             // reset the active menu item and set the next item as "active" (if there is one)
